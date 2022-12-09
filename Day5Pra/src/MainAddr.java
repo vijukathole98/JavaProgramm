1481334bcc619bcb9773948d5393509b41b3248b
Vijay Kathole <85567517+vijukathole98@users.noreply.github.com>
2022-12-09 16:11:38 +0530

    Create MainAddr.java

---
+++ Day5Pra/src/MainAddr.java
@@ -0,0 +1,15 @@
+
+public class MainAddr {
+	public static void main(String args[]){
+		
+	
+	Acc a1=new Acc();
+	System.out.println(a1);
+	
+	Acc a2=new Acc(111,"vijay",123444.3,new Addr("Mumbai",3748),new Date(11,23,343));
+	System.out.println(a2);
+	
+	
+   }
+
+}
